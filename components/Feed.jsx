674ae49eb943e9d57f08401fe81f4c633d31781d
dokesz <lacivagyok3@gmail.com>
2023-11-19 10:51:50 +0100

    look at this conditional rendering in prod

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -24,50 +24,20 @@ const PromptCardList = ({ data, handleTagClick }) => {
 
 const Feed = () => {
   const [searchText, setSearchText] = useState("");
-  const [posts, setPosts] = useState([]);
+  // const [posts, setPosts] = useState([]);
   const [filteredPosts, setFilteredPosts] = useState([]);
   const { data: session } = useSession();
-  const [isLoading, setIsLoading] = useState(true);
+  // const [isLoading, setIsLoading] = useState(true);
   const [groupEvents, setGroupEvents] = useState({});
 
+  const fetcher = (...args) => fetch(...args).then(res => res.json());
+
+  const { data: posts, error, isLoading } = useSWR('/api/szevent', fetcher);
 
   const handleSearchChange = (e) => {
     setSearchText(e.target.value);
   };
 
-  //handleTagClick is a function that will be passed to PromptCardList
-  //and then to PromptCard as a prop so that when the user clicks on a tag
-  //it will filter the posts by that tag
-  const handleTagClick = (tag) => {
-    setSearchText(tag);
-  };
-
-  useEffect(() => {
-    const fetchPosts = async () => {
-      if (isLoading) {
-        try {
-          const response = await fetch("/api/szevent");
-          if (!response.ok) {
-            console.error(`Fetch error: ${response.status} - ${response.statusText}`);
-            // Optionally, log the response body for more details
-            const responseBody = await response.text();
-            console.error(`Response body: ${responseBody}`);
-            throw new Error("Failed to fetch data");
-          }
-          const data = await response.json();
-          setPosts(data);
-        } catch (error) {
-          console.error("Failed to fetch posts:", error);
-        } finally {
-          setIsLoading(false);
-        }
-      }
-    };
-
-    fetchPosts();
-  }, [isLoading]);
-
-  console.log('posts', posts);
 
   useEffect(() => {
     if (posts) {
@@ -83,6 +53,56 @@ const Feed = () => {
     }
   }, [searchText, session, posts]);
 
+  useEffect(() => {
+    // Check if posts is defined before grouping
+    if (posts) {
+      setGroupEvents(groupPostsByTag(posts));
+    } else {
+      // If posts is undefined or null, set groupEvents to an empty object
+      setGroupEvents({});
+    }
+  }, [posts]);
+
+
+  //handleTagClick is a function that will be passed to PromptCardList
+  //and then to PromptCard as a prop so that when the user clicks on a tag
+  //it will filter the posts by that tag
+  const handleTagClick = (tag) => {
+    setSearchText(tag);
+  };
+
+  // useEffect(() => {
+  //   const fetchPosts = async () => {
+  //     if (isLoading) {
+  //       try {
+  //         const response = await fetch("/api/szevent");
+  //         if (!response.ok) {
+  //           console.error(`Fetch error: ${response.status} - ${response.statusText}`);
+  //           // Optionally, log the response body for more details
+  //           const responseBody = await response.text();
+  //           console.error(`Response body: ${responseBody}`);
+  //           throw new Error("Failed to fetch data");
+  //         }
+  //         const data = await response.json();
+  //         setPosts(data);
+  //       } catch (error) {
+  //         console.error("Failed to fetch posts:", error);
+  //       } finally {
+  //         setIsLoading(false);
+  //       }
+  //     }
+  //   };
+
+  //   fetchPosts();
+  // }, [isLoading]);
+
+  // console.log('posts', posts);
+
+  if (isLoading) return <div className="flex text-center mt-4">Az események töltődnek...</div>
+
+  if (error) return <div className="flex text-center mt-4 text-red-700 font-bold">Hiba történt az események betöltése közben, kérlek frissíts rá az oldalra</div>
+
+
   const groupPostsByTag = (posts) => {
     if (posts) {
 
@@ -97,15 +117,6 @@ const Feed = () => {
     }
   };
 
-  useEffect(() => {
-    // Check if posts is defined before grouping
-    if (posts) {
-      setGroupEvents(groupPostsByTag(posts));
-    } else {
-      // If posts is undefined or null, set groupEvents to an empty object
-      setGroupEvents({});
-    }
-  }, [posts]);
 
   return (
     <section className="feed">
