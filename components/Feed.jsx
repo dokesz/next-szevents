b56fa70c58e0aa3bcd07bf9784f48f7899085d11
dokesz <lacivagyok3@gmail.com>
2023-11-19 12:22:27 +0100

    .

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -16,7 +16,20 @@ const Feed = () => {
   const [filteredPosts, setFilteredPosts] = useState([]);
   const [groupEvents, setGroupEvents] = useState({});
 
-  const fetcher = async (url) => await fetch(url, { 'method': 'GET', cache: "default" }).then(res => res.json());
+  const fetcher = async (url) => {
+    const res = await fetch(url);
+
+    if (!res.ok) {
+      const error = new Error('An error occurred while fetching the data.')
+      // Attach extra info to the error object.
+      error.info = await res.json()
+      error.status = res.status
+      throw error
+    }
+
+    return res.json()
+  }
+
   const product_url = '/api/szevent';
   const { data: posts, error, isLoading } = useSWR(product_url, fetcher);
 
