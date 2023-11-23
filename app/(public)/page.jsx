15a9cb3873bb347ce8139f9e83120505ce3407f7
Dóka László <91010700+dokesz@users.noreply.github.com>
2023-11-23 11:58:25 +0100

    suspense loading

--- app/(public)/page.jsx
+++ app/(public)/page.jsx
@@ -1,12 +1,15 @@
 import Test from '@components/Test';
+import Loading from './loading';
+import Suspense from 'react';
 
 
 const Home = async () => {
   return (
     <section className="w-full flex-center flex-col">
       <h1 className="text-2xl font-bold md:mt-4 mt-6 mb-4">Itt láthatod az összes elérhető rendezvényt!</h1>
-
+      <Suspense fallback={<Loading/>}>
       <Test/>
+      </Suspense>
 
     </section>
   );
