f8355d3aebe9afe9691f3e39ab9d4c6bbfe5d8f8
dokesz <lacivagyok3@gmail.com>
2023-11-25 14:02:50 +0100

    min height for home

--- app/(public)/page.jsx
+++ app/(public)/page.jsx
@@ -6,7 +6,7 @@ import { Suspense } from 'react';
 
 const Home = async () => {
   return (
-    <section className="w-full flex-center flex-col">
+    <section className="w-full flex-center flex-col min-h-[1000px]">
       <h1 className="text-2xl font-bold md:mt-4 mt-6 mb-4">Itt láthatod az összes elérhető rendezvényt!</h1>
       <Suspense fallback={<Loading />}>
         <Test />
