5291d348cedb2acf5bebd2ca808b5ec91db7df6a
dokesz <lacivagyok3@gmail.com>
2023-11-25 12:18:19 +0100

    test suspense in client side

--- app/(public)/page.jsx
+++ app/(public)/page.jsx
@@ -1,3 +1,4 @@
+"use client"
 import Test from '@components/Test';
 import Loading from './loading';
 import { Suspense } from 'react';
@@ -7,8 +8,8 @@ const Home = async () => {
   return (
     <section className="w-full flex-center flex-col">
       <h1 className="text-2xl font-bold md:mt-4 mt-6 mb-4">Itt láthatod az összes elérhető rendezvényt!</h1>
-      <Suspense fallback={<Loading/>}>
-      <Test/>
+      <Suspense fallback={<Loading />}>
+        <Test />
       </Suspense>
 
     </section>
