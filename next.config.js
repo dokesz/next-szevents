ff1316f6abb277e0250bea34a81453dac0ebe8dc
dokesz <lacivagyok3@gmail.com>
2023-11-25 12:48:34 +0100

    go back without a canary version

--- next.config.js
+++ next.config.js
@@ -2,9 +2,9 @@
 const nextConfig = {}
 
 module.exports = {
-  experimental: {
-    ppr: true,
-  },
+  // experimental: {
+  //   ppr: true,
+  // },
   images: {
     domains: ['lh3.googleusercontent.com', 'utfs.io'],
   },
